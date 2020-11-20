5709b04e2d4b4bda55a4e5e505d0dd1defaacf01
Hannah-GOng <65155953+Hannah-GOng@users.noreply.github.com>
2020-11-20 01:34:21 -0500

    Update application.py

--- application.py
+++ application.py
@@ -2,12 +2,25 @@ from flask import Flask, render_template, request
 import boto3
 import json
 
-# detect the celebrities
-def recognize_celebrities(photo):
-    client=boto3.client('rekognition')
 
-    with open(photo, 'rb') as image:
-        response = client.recognize_celebrities(Image={'Bytes': image.read()})
+@application.route('/upload', methods=['POST', 'GET'])
+
+# detect the celebrities
+def recognize_celebrities():
+    file = request.files['file']
+    bucket = ''
+    
+    # create a resource of S3 to use 'Bucket' attribute
+    s3_resource = boto3.resource('s3')
+
+    # upload the file as on object using put_object
+    s3_resource.Bucket(bucket).put_object(Key=file.filename, Body=file)
+    
+    Image= {'S3Object': {'Bucket': bucket, 'Name': file.filename}}
+    
+    # Celebrity Recognition by AWS Rekognition
+    client=boto3.client('rekognition')
+    response = client.recognize_celebrities(Image={'Bytes': image.read()})
 
     print('Detected faces for ' + photo)    
     for celebrity in response['CelebrityFaces']:
